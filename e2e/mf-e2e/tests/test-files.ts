08e728a85e3af9e41843b31ce5d6278ebc2ddde5
Anton Todorov <shadowtabris84@gmail.com>
2020-11-28 12:51:52 +0200

    fix  for mf-e2e

--- e2e/mf-e2e/tests/test-files.ts
+++ e2e/mf-e2e/tests/test-files.ts
@@ -236,6 +236,20 @@ export const angularJson = `
     "packageManager": "yarn"
   }
 }
-`
+`;
 
+export const mainTsContent = `
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+`;
