69c0100aeb7d73de478d6e3b1e150bdcf461c8bc
Manfred Steyer <manfred.steyer@gmx.net>
2021-06-05 23:45:45 +0200

    mf-tools: export startsWith

--- libs/mf-tools/src/lib/web-components/router-utils.ts
+++ libs/mf-tools/src/lib/web-components/router-utils.ts
@@ -1,5 +1,15 @@
 import { Router, UrlMatcher, UrlSegment } from '@angular/router';
 
+export function startsWith(prefix: string): UrlMatcher {
+    return (url: UrlSegment[]) => {
+        const fullUrl = url.map(u => u.path).join('/');
+        if (fullUrl.startsWith(prefix)) {
+            return ({ consumed: url});
+        }
+        return null;
+    };
+}
+
 export function endsWith(prefix: string): UrlMatcher {
     return (url: UrlSegment[]) => {
         const fullUrl = url.map(u => u.path).join('/');
