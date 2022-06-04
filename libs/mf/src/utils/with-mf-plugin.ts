623837c29052d441cde6ee5940ac54d7e868dce2
Manfred Steyer <manfred.steyer@gmx.net>
2022-06-04 18:03:18 +0200

    feat(mf): add support for eager and pinned

--- libs/mf/src/utils/with-mf-plugin.ts
+++ libs/mf/src/utils/with-mf-plugin.ts
@@ -1,5 +1,6 @@
 import { findRootTsConfigJson, shareAll } from './share-utils';
 import { SharedMappings } from './shared-mappings';
+import { ModifyEntryPlugin } from './modify-entry-plugin';
 
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
@@ -11,36 +12,12 @@ export function withModuleFederationPlugin(config: unknown) {
     const mappings = new SharedMappings();
     mappings.register(findRootTsConfigJson(), sharedMappings);
 
-    if (!config['library']) {
-        config['library'] = {
-            type: 'module'
-        }
-    }
-
-    if (!config['filename']) {
-        config['filename'] = 'remoteEntry.js';
-    }
-
-    if (!config['shared']) {
-        config['shared'] = shareAll(
-            { singleton: true, strictVersion: true, requiredVersion: 'auto'});
-    }
-
-    if (typeof config['shared'] === 'object') {
-        config['shared'] = {
-            ...config['shared'],
-            ...mappings.getDescriptors()
-        }
-    }
-
-    if (Array.isArray(config['shared'])) {
-        config['shared'] = [
-            ...config['shared'],
-            mappings.getDescriptors()
-        ]
-    }
+    setDefaults(config, mappings);
+    const modifyEntryPlugin = createModifyEntryPlugin(config);
 
     const isModule = config['library']?.['type'] === 'module';
+
+    // console.log('sharedConfig.modFed', sharedConfig.modFed);
 
     return {
         output: {
@@ -61,7 +38,80 @@ export function withModuleFederationPlugin(config: unknown) {
         } : {},
         plugins: [
           new ModuleFederationPlugin(config),
-          mappings.getPlugin()
+          mappings.getPlugin(),
+          ...(modifyEntryPlugin) ? [modifyEntryPlugin] : []
         ],
       };
 }  
+
+function setDefaults(config: unknown, mappings: SharedMappings) {
+  if (!config['library']) {
+    config['library'] = {
+      type: 'module'
+    };
+  }
+
+  if (!config['filename']) {
+    config['filename'] = 'remoteEntry.js';
+  }
+
+  if (!config['shared']) {
+    config['shared'] = shareAll(
+      { singleton: true, strictVersion: true, requiredVersion: 'auto' });
+  }
+
+  if (typeof config['shared'] === 'object') {
+    config['shared'] = {
+      ...config['shared'],
+      ...mappings.getDescriptors()
+    };
+  }
+
+  if (Array.isArray(config['shared'])) {
+    config['shared'] = [
+      ...config['shared'],
+      mappings.getDescriptors()
+    ];
+  }
+}
+
+function createModifyEntryPlugin(config: unknown) {
+  const pinned = [];
+  const eager = [];
+  for (const key in config['shared']) {
+    const entry = config['shared'][key];
+    if (entry.pinned) {
+      pinned.push(key);
+      delete entry.pinned;
+    }
+    if (entry.eager) {
+      eager.push(key);
+    }
+  }
+  const hasPinned = pinned.length > 0;
+  const hasEager = eager.length > 0;
+
+  if (hasPinned && config['remotes']) {
+    throw new Error([
+      'Pinned dependencies in combination with build-time remotes are not allowed. ',
+      'Either remove "pinned: true" from all shared dependencies or delete all ',
+      'remotes in your webpack config and use runtime remote loading instead.'
+    ].join(''));
+  }
+
+  let modifyEntryConfig = {};
+  let modifyEntryPlugin = null;
+  if (hasPinned) {
+    modifyEntryConfig['main'] = { import: pinned };
+  }
+
+  if (hasEager) {
+    modifyEntryConfig['styles'] = { dependOn: ['main'] };
+    modifyEntryConfig['polyfills'] = { dependOn: ['main'] };
+  }
+
+  if (hasPinned || hasEager) {
+    modifyEntryPlugin = new ModifyEntryPlugin(modifyEntryConfig);
+  }
+  return modifyEntryPlugin;
+}
