b8dd205d411bbb7699186d11891651aed087bf8a
Manfred Steyer <manfred.steyer@gmx.net>
2022-09-11 22:14:43 +0200

    feat(nf-runtime): don't fail bootstrapping if remote entry cannot be loaded

--- update-local.bat
+++ update-local.bat
@@ -1,6 +1,11 @@
 call npm unpublish @softarc/native-federation@1.0.0 --registry http://localhost:4873
+call npm unpublish @softarc/native-federation-runtime@1.0.0 --registry http://localhost:4873
+
 call npm unpublish @angular-architects/native-federation@1.0.0-beta.1 --registry http://localhost:4873
 call nx build native-federation
 call nx build native-federation-core
+call nx build native-federation-runtime
+
 call npm publish dist\libs\native-federation-core --registry http://localhost:4873
 call npm publish dist\libs\native-federation --registry http://localhost:4873
+call npm publish dist\libs\native-federation-runtime --registry http://localhost:4873
