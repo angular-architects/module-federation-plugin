6c2eb6e1b715c1c4e4790f1d06a4623df44b6a29
Manfred Steyer <manfred.steyer@gmx.net>
2022-09-23 13:12:43 +0200

    fix(nf): make terser in rollup fallback less aggresive

--- libs/native-federation/src/utils/rollup.ts
+++ libs/native-federation/src/utils/rollup.ts
@@ -42,7 +42,7 @@ export async function runRollup(
             'process.env.NODE_ENV': '"development"',
           },
         }),
-        terser(),
+        terser({ mangle: false, format: { comments: false } }),
       ],
     });
 
