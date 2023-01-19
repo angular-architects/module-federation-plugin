03cb3d3e6ee89bb4c08ecec4d320076326d6e426
arpadbozzay <bozzay.arpad@gmail.com>
2023-01-19 10:33:58 +0100

    use modify entry plugin in after env hook

--- libs/mf/src/utils/modify-entry-plugin.ts
+++ libs/mf/src/utils/modify-entry-plugin.ts
@@ -1,3 +1,4 @@
+const PLUGIN_NAME = 'modify-entry-plugin';
 export class ModifyEntryPlugin {
   config: unknown;
   constructor(config) {
@@ -5,26 +6,41 @@ export class ModifyEntryPlugin {
   }
 
   apply(compiler) {
-    const mergeEntry = (keyFn, key) => [
-      ...(keyFn(this.config[key]) || []),
-      ...(keyFn(compiler.options.entry[key]) || []),
-    ];
-    const cfgOrRemove = (objFn, valueFn, key) => {
-      const values = mergeEntry(valueFn, key);
-      return values.length > 0 ? objFn(values) : {};
-    };
-    Object.keys(this.config).forEach((key) => {
-      compiler.options.entry[key] = {
-        ...cfgOrRemove(
-          (v) => ({ import: v }),
-          (c) => c.import,
-          key
-        ),
-        ...cfgOrRemove(
-          (v) => ({ dependOn: v }),
-          (c) => c.dependOn,
-          key
-        ),
+    compiler.hooks.afterEnvironment.tap(PLUGIN_NAME, () => {
+      const webpackOptions = compiler.options;
+      const entry =
+        typeof webpackOptions.entry === 'function'
+          ? webpackOptions.entry()
+          : webpackOptions.entry;
+
+      webpackOptions.entry = async () => {
+        const entries = await entry;
+
+        const mergeEntry = (keyFn, key) => [
+          ...(keyFn(this.config[key]) || []),
+          ...(keyFn(entries[key]) || []),
+        ];
+        const cfgOrRemove = (objFn, valueFn, key) => {
+          const values = mergeEntry(valueFn, key);
+          return values.length > 0 ? objFn(values) : {};
+        };
+
+        Object.keys(this.config).forEach((key) => {
+          entries[key] = {
+            ...cfgOrRemove(
+              (v) => ({ import: v }),
+              (c) => c.import,
+              key
+            ),
+            ...cfgOrRemove(
+              (v) => ({ dependOn: v }),
+              (c) => c.dependOn,
+              key
+            ),
+          };
+        });
+
+        return entries;
       };
     });
   }
