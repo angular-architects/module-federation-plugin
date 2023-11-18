813b6d9ab5275733e59258e3368c3b1a37caeb7e
mchlbrnd <michael.wolbert@gmail.com>
2023-11-18 21:08:29 +0100

    allow federation-config as esm and sync/async exporting of config

--- libs/native-federation-core/src/lib/config/share-utils.ts
+++ libs/native-federation-core/src/lib/config/share-utils.ts
@@ -1,5 +1,5 @@
-import path = require('path');
-import fs = require('fs');
+import path from 'path';
+import fs from 'fs';
 import { cwd } from 'process';
 import { SharedConfig } from './federation-config';
 import {
