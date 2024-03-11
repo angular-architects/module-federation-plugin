3f1dcfae56be74b82d8c5aeef48f8a7746ad7c20
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-11 10:52:14 +0100

    fix(nf): Inject `importmap-shim` to the `<head>` instead of the end of the `<body>`

--- libs/native-federation-runtime/src/lib/utils/add-import-map.ts
+++ libs/native-federation-runtime/src/lib/utils/add-import-map.ts
@@ -1,7 +1,7 @@
 import { ImportMap } from '../model/import-map';
 
 export function appendImportMap(importMap: ImportMap) {
-  document.body.appendChild(
+  document.head.appendChild(
     Object.assign(document.createElement('script'), {
       type: 'importmap-shim',
       innerHTML: JSON.stringify(importMap),
