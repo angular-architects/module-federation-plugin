806e1efd6046fdc329b2aa496b97c59e6c23ead0
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-12 16:49:26 +0100

    fix(test): fix jest tests

--- apps/playground-e2e/src/integration/app.spec.ts
+++ apps/playground-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('playground', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to playground!');
+    getGreeting().contains('Hello Native Federation!');
   });
 });
