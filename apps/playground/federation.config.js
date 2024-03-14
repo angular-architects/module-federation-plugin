b9fd4d29da2edbc33eb901e82639e43f52c85690
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-14 11:52:12 +0100

    chore(nx): Upgrade to Nx 18 - align apps

--- apps/playground/federation.config.js
+++ apps/playground/federation.config.js
@@ -1,16 +1,17 @@
-const {
-  withNativeFederation,
-  shareAll,
-} = require('@angular-architects/native-federation/config');
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
+
   shared: {
-    ...shareAll({
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: 'auto',
-    }),
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
 
-  sharedMappings: ['@angular-architects/playground-lib'],
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    // Add further packages you don't need at runtime
+  ]
+  
 });
