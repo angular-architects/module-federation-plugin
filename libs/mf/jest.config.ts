098947faf0131e8d6e1d1f275e13722d84ebccaf
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-03-14 12:26:04 +0100

    chore(nx): Upgrade to Nx 18 - align js/tsc configs

--- libs/mf/jest.config.ts
+++ libs/mf/jest.config.ts
@@ -2,11 +2,10 @@
 export default {
   displayName: 'mf',
   preset: '../../jest.preset.js',
-  globals: {},
-  transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/mf',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/mf',
 };
