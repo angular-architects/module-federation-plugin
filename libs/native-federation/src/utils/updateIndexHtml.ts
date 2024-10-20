174966a23905285712a313ce2c867bc03755bcad
Dmytro Vradii <wiggledima@gmail.com>
2024-10-20 17:23:00 +0300

    correct the index file path for SSR builds in the updateIndexHtml util

--- libs/native-federation/src/utils/updateIndexHtml.ts
+++ libs/native-federation/src/utils/updateIndexHtml.ts
@@ -8,7 +8,7 @@ export function updateIndexHtml(
   nfOptions: NfBuilderSchema
 ) {
   const outputPath = path.join(fedOptions.workspaceRoot, fedOptions.outputPath);
-  const indexPath = path.join(outputPath, 'index.html');
+  const indexPath = path.join(outputPath, nfOptions.ssr ? 'index.csr.html' : 'index.html');
   const mainName = fs
     .readdirSync(outputPath)
     .find((f) => f.startsWith('main') && f.endsWith('.js'));
