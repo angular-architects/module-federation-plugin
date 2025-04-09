1e43b08283cb09ed4fa9bd5d13cc4a50e8e5090e
Manfred Steyer <manfred.steyer@gmx.net>
2025-04-09 18:35:31 +0200

    refactor(nf): replace logger by own implementation

--- libs/native-federation-core/src/lib/utils/logger.ts
+++ libs/native-federation-core/src/lib/utils/logger.ts
@@ -5,12 +5,14 @@ import chalk from 'chalk';
 let verbose = false;
 
 export const logger = {
-  warn: (msg: any)    => console.warn(chalk.bgYellow.ansi256(15)(' WARN '), msg),
-  error: (msg: any)   => console.error(chalk.bgRed.ansi256(15)(' ERRR '), msg),
+  warn: (msg: any) => console.warn(chalk.bgYellow.ansi256(15)(' WARN '), msg),
+  error: (msg: any) => console.error(chalk.bgRed.ansi256(15)(' ERRR '), msg),
   notice: (msg: any) => console.log(chalk.bgYellowBright.black(' NOTE '), msg),
   info: (msg: any) => console.log(chalk.bgGreen.ansi256(15)(' INFO '), msg),
-  verbose: (msg: any) => verbose && console.log(chalk.bgGreen.ansi256(15)(' DBG! '), msg),
-  debug: (msg: any) => verbose && console.log(chalk.bgGreen.ansi256(15)(' DBG! '), msg),
+  verbose: (msg: any) =>
+    verbose && console.log(chalk.bgGreen.ansi256(15)(' DBG! '), msg),
+  debug: (msg: any) =>
+    verbose && console.log(chalk.bgGreen.ansi256(15)(' DBG! '), msg),
 };
 
 export const setLogLevel = (level: string) => {
