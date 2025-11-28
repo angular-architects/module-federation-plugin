7316c18ee0ace23accc61659a37f94d19495a76e
Alejandro <aramirezj@godmodels.com>
2025-11-28 19:56:04 +0100

    fix(native-federation-node): renames function usage from native-federation-node

--- libs/native-federation-node/src/lib/node/init-node-federation.ts
+++ libs/native-federation-node/src/lib/node/init-node-federation.ts
@@ -1,15 +1,13 @@
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
 import * as fs from 'node:fs/promises';
+import { register } from 'node:module';
 import * as path from 'node:path';
+import { pathToFileURL } from 'node:url';
 
 import {
   FederationInfo,
-  ImportMap,
-  InitFederationOptions,
-  mergeImportMaps,
-  processHostInfo,
-  processRemoteInfos,
+  fetchAndRegisterRemotes,
+  ImportMap, mergeImportMaps,
+  processHostInfo
 } from '@softarc/native-federation-runtime';
 import { IMPORT_MAP_FILE_NAME } from '../utils/import-map-loader';
 import { resolver } from '../utils/loader-as-data-url';
@@ -54,7 +52,7 @@ async function createNodeImportMap(
 
   const hostInfo = await loadFsFederationInfo(relBundlePath);
   const hostImportMap = await processHostInfo(hostInfo, './' + relBundlePath);
-  const remotesImportMap = await processRemoteInfos(remotes, {
+  const remotesImportMap = await fetchAndRegisterRemotes(remotes, {
     throwIfRemoteNotFound: options.throwIfRemoteNotFound,
     cacheTag: options.cacheTag,
   });
