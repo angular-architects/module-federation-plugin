c25614a4b8bfdf129942d2e719ce516236479f27
Aukevanoost <auke.v.o@hotmail.com>
2026-01-20 10:56:49 +0100

    fix: Added legacy support

--- libs/native-federation/src/utils/angular-locales.ts
+++ libs/native-federation/src/utils/angular-locales.ts
@@ -2,20 +2,24 @@ import { share, SharedConfig } from '@softarc/native-federation/build';
 
 export function shareAngularLocales(
   keys,
-  config: SharedConfig = {
-    singleton: true,
-    strictVersion: true,
-    requiredVersion: 'auto',
-  },
+  opts: { config?: SharedConfig; legacy?: boolean },
 ) {
+  if (!opts.config) {
+    opts.config = {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    };
+  }
+  const ext = !!opts.legacy ? '.mjs' : '.js';
   return keys.reduce((acc, key) => {
     acc[`@angular/common/locales/${key}`] = {
-      ...config,
-      packageInfo: config.packageInfo || {
-        ...config.packageInfo,
+      ...opts.config!,
+      packageInfo: opts.config.packageInfo || {
+        ...opts.config.packageInfo,
         entryPoint:
-          config.packageInfo?.entryPoint ||
-          `node_modules/@angular/common/locales/${key}.js`,
+          opts.config.packageInfo?.entryPoint ||
+          `node_modules/@angular/common/locales/${key}${ext}`,
       },
     };
     return share(acc);
